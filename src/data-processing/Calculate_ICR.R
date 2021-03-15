6fcb27b3cf8147b3f51d6ede4c8d148ab41e603e
MarikenvdVelden <m.a.c.g.vander.velden@vu.nl>
2021-03-15 08:59:31 +0100

    add icr

--- src/data-processing/Calculate_ICR.R
+++ src/data-processing/Calculate_ICR.R
@@ -2,7 +2,7 @@
 rm(list=ls())
 library(here)
 library(tidyverse)
-library(googlesheets4)
+library(irr)
 
 d <- read_csv(here("data/raw/coded-data-NL-2017-campaign.csv")) %>%
   mutate(id = paste(date, party, sep = "_")) %>%
@@ -11,3 +11,11 @@ d <- read_csv(here("data/raw/coded-data-NL-2017-campaign.csv")) %>%
 s <- sample_n(d, 200)
 write_csv(s, here("data/intermediate/Subset_data_intercoder_reliability.csv"))
 
+# Calculate ICR on issues
+d <- read_csv(here("data/intermediate/Subset_data_intercoder_reliability.csv"))
+
+neg <- matrix(c(d$neg1, d$neg2), ncol = 2)
+kappa2(neg)
+
+issue <- matrix(c(d$issue_theme1, d$issue_theme2), ncol = 2)
+kappa2(issue)
